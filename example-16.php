b94bb58df9dae9ced478dde0d049741a8d280f00
Ralph Schindler <ralphschindler@yahoo.com>
2012-01-09 15:18:59 -0600

    Added example number 16 (Builder definition example) Added beta2 phars with include director for out-the-box runs.

---
+++ example-16.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MovieApp {
+    class Lister {
+        public $finder;
+        public function __construct(Finder $finder){
+            $this->finder = $finder;
+        }
+    }
+    class Finder {
+        
+    }
+}
+
+namespace {
+    // bootstrap
+    include 'zf2bootstrap' . ((stream_resolve_include_path('zf2bootstrap.php')) ? '.php' : '.dist.php');
+
+    use Zend\Di\Di,
+        Zend\Di\DefinitionList,
+        Zend\Di\Definition\BuilderDefinition;
+    
+    $builderDefinition = new BuilderDefinition();
+    $builderDefinition->createClassesFromArray(array(
+        'MovieApp\Lister' => array(
+            'methods' => array(
+                '__construct' => array(
+                    'repo' => 'MovieApp\Finder'
+                )
+            )
+        ),
+        'MovieApp\Finder' => array()
+    ));
+    
+    $di = new Di(new DefinitionList(array($builderDefinition)));
+    $lister = $di->get('MovieApp\Lister');
+
+    // expression to test
+    $works = ($lister->finder instanceof MovieApp\Finder);
+
+    // display result
+    echo (($works) ? 'It works!' : 'It DOES NOT work!');
+
+
+}
