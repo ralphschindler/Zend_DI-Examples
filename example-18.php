2e1ae699d8b589e2c8d5e593916651bfbec4edaf
Ralph Schindler <ralphschindler@yahoo.com>
2012-01-10 11:28:56 -0600

    Added example for supertype checking

---
+++ example-18.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MovieApp {
+    class AbstractLister {
+        public $finder;
+        public function setFinder(Finder $finder){
+            $this->finder = $finder;
+        }
+    }
+    class Lister extends AbstractLister {}
+    class Finder {
+    }
+}
+
+namespace {
+    // bootstrap
+    include 'zf2bootstrap' . ((stream_resolve_include_path('zf2bootstrap.php')) ? '.php' : '.dist.php');
+
+    $di = new Zend\Di\Di;
+    $di->configure(new Zend\Di\Configuration(array(
+        'definition' => array(
+            'class' => array(
+                'MovieApp\AbstractLister' => array(
+                    'setFinder' => array('required' => true)
+                )
+            )
+        )
+    )));
+    $lister = $di->get('MovieApp\Lister');
+    
+    // expression to test
+    $works = ($lister->finder instanceof MovieApp\Finder);
+
+    // display result
+    echo (($works) ? 'It works!' : 'It DOES NOT work!');
+}
