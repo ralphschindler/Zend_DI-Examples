ff4bcebe16b2d979412291ec655543b583fb6a9f
Ralph Schindler <ralphschindler@yahoo.com>
2012-04-18 14:35:53 -0500

    Added example for injection after instantiation only.

---
+++ example-21.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MovieApp {
+    
+    class Lister implements FinderAwareInterface {
+        public $finder;
+        public function setFinder(Finder $finder){
+            $this->finder = $finder;
+        }
+    }
+    
+    class Finder {
+        public function findAllByName($name) {}
+    }
+    
+    interface FinderAwareInterface {
+        public function setFinder(Finder $finder);
+    }
+}
+
+namespace {
+    // bootstrap
+    include 'zf2bootstrap' . ((stream_resolve_include_path('zf2bootstrap.php')) ? '.php' : '.dist.php');
+
+    $di = new Zend\Di\Di;
+    $lister = new MovieApp\Lister;
+
+var_dump($lister);
+
+    $di->injectDependencies($lister);
+
+var_dump($lister);
+
+    // expression to test
+    $works = ($lister->finder instanceof MovieApp\Finder);
+
+    // display result
+    echo (($works) ? 'It works!' : 'It DOES NOT work!') . PHP_EOL;
+
+}
